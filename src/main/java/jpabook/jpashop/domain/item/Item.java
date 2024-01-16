dbd3778655e9034a4dc9ac679f75203a7b43d44f
LSBsity <dltmdqls51@naver.com>
2024-01-17 05:28:33 +0900

    Item Entity

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -34,7 +34,7 @@ public abstract class Item {
     /**
      * stock 증가
      */
-    public void addQuantity(int quantity) {
+    public void addStock(int quantity) {
         this.stockQuantity += quantity;
     }
 
