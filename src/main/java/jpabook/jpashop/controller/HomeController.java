5ce852f926cadb49fff48e9712f734251223b14b
LSBsity <dltmdqls51@naver.com>
2024-01-18 05:37:23 +0900

    Home Controller

---
+++ src/main/java/jpabook/jpashop/controller/HomeController.java
@@ -0,0 +1,18 @@
+package jpabook.jpashop.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@Slf4j
+public class HomeController {
+
+    @RequestMapping("/home")
+    public String home() {
+        log.info("home controller");
+        return "home";
+    }
+
+
+}
