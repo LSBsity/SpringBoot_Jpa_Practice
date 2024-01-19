641b139f1f2a97fac5338f9c7619bb215a50b923
LSBsity <dltmdqls51@naver.com>
2024-01-19 19:30:58 +0900

    Member Entity

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,6 +1,8 @@
 package jpabook.jpashop.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -17,11 +19,13 @@ public class Member {
     @Column(name = "member_id")
     private Long id;
 
+    @NotEmpty
     private String name;
 
     @Embedded
     private Address address;
 
+    @JsonIgnore // json에 이 필드를 내보내지 않음
     @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<>();
 
